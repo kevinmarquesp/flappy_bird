05c4a8a6d7830557ff7dbd957cf3b9e4c75e23e7
kevin <kevinmarqueskmp@gmail.com>
2021-12-28 17:40:49 -0300

    Buttons and easter egg

--- Script/Profile.js
+++ Script/Profile.js
@@ -1,8 +1,15 @@
 const profile_info = document.querySelector("#profile_info");
 const menu_button = document.querySelector("#menu_button");
 
+const easter_egg = document.querySelector("#easter_egg");
+const easter_egg_button = document.querySelector(".easter-egg-btn");
+
 
 menu_button.onclick = () => {
   profile_info.classList.toggle("active");
   menu_button.classList.toggle("active");
 };
+
+easter_egg_button.onclick = () => {
+  easter_egg.classList.toggle("hide")
+}
